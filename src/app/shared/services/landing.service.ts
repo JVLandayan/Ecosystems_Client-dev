0493cb8cb444f64859ab8b3503f4dd5c5176cb59
JVLandayan <57561054+JVLandayan@users.noreply.github.com>
2021-04-26 20:02:25 +0800

    first commit

---
+++ src/app/shared/services/landing.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Merchandise } from '../models/merchandise.model';
+import { map } from 'rxjs/operators';
+import { Teams } from '../models/teams.model';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'tinymce';
+import { Articles } from '../models/articles.model';
+import { Author } from '../models/author.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LandingService {
+  constructor(private http: HttpClient) {}
+
+  //merch-fetch-data
+  FETCH_merchandise() {
+    return this.http.get<Merchandise[]>(environment.apiUrl + 'merchandise');
+  }
+
+  //teams-fetch-data
+  FETCH_teams() {
+    return this.http.get<Teams[]>(environment.apiUrl + 'teams');
+  }
+
+  FETCH_articles() {
+    return this.http.get<Articles[]>(environment.apiUrl + 'articles');
+  }
+
+  FETCH_article(id) {
+    return this.http.get<Articles>(environment.apiUrl + 'articles/' + id);
+  }
+
+  FETCH_articleAuthor(id) {
+    return this.http.get<Author>(environment.apiUrl + 'accounts/author/' + id);
+  }
+}
